ec23103976b231c35468581bb444bf268cd5346f
gquesnot <gaqu1994@gmail.com>
2025-10-25 18:50:36 +0200

    fix merge

--- server_fn/src/codec/mod.rs
+++ server_fn/src/codec/mod.rs
@@ -56,10 +56,6 @@ mod postcard;
 #[cfg(feature = "postcard")]
 pub use postcard::*;
 
-#[cfg(feature = "bitcode")]
-mod bitcode;
-#[cfg(feature = "bitcode")]
-pub use bitcode::*;
 
 mod patch;
 pub use patch::*;
